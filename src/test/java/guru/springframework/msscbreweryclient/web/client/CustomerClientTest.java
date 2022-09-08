b594e130b7f41d1f33ffb1d3aa5ca99195c5ef74
Felix Kubli <felix.kubli@ti8m.ch>
2022-09-08 09:38:31 +0200

    add customer client and tests

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/CustomerClientTest.java
@@ -0,0 +1,43 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//Integration Tests
+@SpringBootTest
+class CustomerClientTest {
+
+    @Autowired
+    CustomerClient client;
+
+    @Test
+    void getCustomerById() {
+        CustomerDto dto = client.getCustomerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void createCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("Max Muster").build();
+        URI uri = client.createCustomer(customerDto);
+        assertNotNull(uri);
+    }
+
+    @Test
+    void updateCustomer() {
+        CustomerDto customerDto = CustomerDto.builder().name("Max Muster").build();
+        client.updateCustomer(UUID.randomUUID(), customerDto);
+    }
+
+    @Test
+    void deleteCustomer() {
+        client.deleteCustomer(UUID.randomUUID());
+    }
+}
